52ecd47345904cdb4a0cae59a0255e201037d9cf
debskiszymon <debski.szymon@gmail.com>
2020-10-06 11:34:39 +0200

    magazyn.py

--- magazyn.py
+++ magazyn.py
@@ -1,6 +1,6 @@
 
 items = [
-    {"Name":"Milk", "Quantity": 120, "Unit": "l", "Unit Price (PLN)": 2.3},
+    {"Name":"Milk", "Quantity": 120.111, "Unit": "l", "Unit Price (PLN)": 2.3},
     {"Name":"Sugar", "Quantity": 1000, "Unit": "kg", "Unit Price (PLN)": 3},
     {"Name":"Flour", "Quantity": 12000, "Unit": "kg", "Unit Price (PLN)": 1.2},
     {"Name":"Coffee", "Quantity": 25, "Unit": "kg", "Unit Price (PLN)": 40}
@@ -13,11 +13,11 @@ sold_items = [
     {"Name":"Coffee", "Quantity": 0, "Unit": "kg", "Unit Price (PLN)": 40}]
 
 def get_items(items):
-    print("{:<10s}{:>7s}{:>9s}{:>20s}".format("Name","Quantity","Unit","Unit Price (PLN)"))
+    print("{:<10s}{:>9s}{:>12s}{:>23s}".format("Name","Quantity","Unit","Unit Price (PLN)"))
     dash = "-" * 47
     print(dash)
     for i in range(len(items)):
-        print("{:<10s}{:>7d}{:>9s}{:>14.1f}".format(list(items[i].values())[0],list(items[i].values())[1],list(items[i].values())[2],list(items[i].values())[3]))
+        print("{:<10s}{:>9.2f}{:>12s}{:>14.2f}".format(list(items[i].values())[0],list(items[i].values())[1],list(items[i].values())[2],list(items[i].values())[3]))
 
 def add_item(items):
     print("Adding to warhouse...")
@@ -41,8 +41,24 @@ def sell_item(items, sold_items):
     #print(sold_items)
     get_items(items)
 
+def get_costs(items):
+    costs = round(sum([items[i]["Quantity"] * items[i]["Unit Price (PLN)"] for i in range(len(items))]), 2)
+    return costs
+
+def get_income(sold_items):
+    income = round(sum([sold_items[i]["Quantity"] * sold_items[i]["Unit Price (PLN)"] for i in range(len(sold_items))]), 2)
+    return income
+
+def show_revenue(costs, income):
+    print("Revenue break down (PLN): ")
+    print(f"Income: {income}")
+    print(f"Costs: {costs}")
+    print("-" * 10)
+    print(f"Revenue: {income - costs}")
+
 def menu(items):
-    message = print("What would you like to do?: ")
+    message = print("What would you like to do? (for commands write commands): ")
+    commands = "show, exit, add, sell, show revenue"
     command = input().lower()
     if command == "exit":
         print("Exiting.... Bye!!")
@@ -53,9 +69,13 @@ def menu(items):
         add_item(items)
     elif command == "sell":
         sell_item(items, sold_items)
+    elif command == "show revenue":
+        costs = get_costs(items)
+        income = get_income(sold_items)
+        show_revenue(costs, income)
+    elif command == "commands":
+        print(commands)
     return message
-
-
 
 
 if __name__ == "__main__":
